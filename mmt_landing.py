47b6fcd0adadef9011a884f916542afea673aa09
Devarshi_Singh <devarshi@self.com>
2024-02-20 20:50:54 +0530

    12

--- mmt_landing.py
+++ mmt_landing.py
@@ -1,16 +1,11 @@
 import os
+import time
+import requests
 from selenium import webdriver
-
-get_path = os.getcwd()
-relative_path = "chromedriver-mac-arm64"
-chromeDriver = "chromedriver"
-
-path_for_chrome_driver = os.path.join(get_path, relative_path, chromeDriver)
-# print(path_for_chrome_driver)
 
 base_url = "https://www.makemytrip.com/"
 
-driver = webdriver.Chrome(path_for_chrome_driver)
+driver = webdriver.Chrome()
 driver.get(base_url)
-
-driver.close()
+time.sleep(20)
+driver.quit()
