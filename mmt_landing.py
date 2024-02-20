7cde84ac2c91efd3637461e76e8bb7e5b7386eda
Devarshi_Singh <devarshi@self.com>
2024-02-20 23:03:22 +0530

    4

--- mmt_landing.py
+++ mmt_landing.py
@@ -26,22 +26,25 @@ def is_pop_up():
     """
     <span class="bgProperties  overlayCrossIcon icon20" style="background-image: url(&quot;//jsak.mmtcdn.com/flights/assets/media/cross-icon.8b0f8487.png&quot;);"></span>
     """
-    print("Func called")
+    # print("Func called")
     wait_for_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div[2]/div/div/div[3]/button", True, wait_time=60)
-    print("Func Exit")
+    # print("Func Exit")
 
-driver = webdriver.Chrome()
-driver.get(base_url)
-print(driver.title)
-# time.sleep(20)
-try:
-    click_on_search()
-    time.sleep(10)
-    is_pop_up()
-    time.sleep(60)
+def main():
+    driver = webdriver.Chrome()
+    driver.get(base_url)
+    print(driver.title)
+    # time.sleep(20)
+    try:
+        click_on_search()
+        time.sleep(10)
+        is_pop_up()
+        time.sleep(60)
 
-except:
-    print("Error Occured")
-finally:
-    driver.quit()
+    except:
+        print("Error Occured")
+    finally:
+        driver.quit()
 
+if __name__ == '__main__':
+    main()
